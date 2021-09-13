91c311e6286caf9424cdf8e91d14a2ce8d8a6620
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-09-13 18:02:39 -0300

    Experimental autorization admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,5 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 
 export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
-  
+  // Verify user ensureAdmin
+  const admin = false
+
+  if(admin) {
+    return next()
+  }
+
+  return response.status(401).json({
+    error: "User is not admin",
+  })
 }
