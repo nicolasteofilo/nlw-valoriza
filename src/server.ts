40e29e170490390183c7478900ef0bf2bb9db7ea
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2021-09-13 17:08:35 -0300

    Dealing error

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
 import 'reflect-metadata'
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
+import 'express-async-errors'
 
 import { router } from './routes';
+import './database'
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
 
-import './database'
+
+app.use((err: Error, request: Request, response: Response, next:NextFunction) => {
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+
+  }
+  return response.status(500).json({
+    status: "error",
+    message: "Internal error server"
+  })
+})
+
+
 app.listen(3333, () => {
-  console.log('Rodando')
+  console.log('🚀 Server started')
 })
